4b46d0f19f6f6fa404984153b4a56fde8318152d
vio-lin <846271633@qq.com>
2017-05-04 09:32:26 +0800

    添加多基站定位的数据包的存储

--- GpsServer/src/com/carPature/test/TestDB.java
+++ GpsServer/src/com/carPature/test/TestDB.java
@@ -8,9 +8,11 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.carPature.dao.DeviceLocationInfoDAO;
+import com.carPature.dao.DeviceLocationLbsInfoDAO;
 import com.carPature.dao.DeviceStatusDAO;
 import com.carPature.dao.UsersDAO;
 import com.carPature.entity1.DeviceLocationInfo;
+import com.carPature.entity1.DeviceLocationLbsInfo;
 import com.carPature.entity1.DeviceStatus;
 import com.carPature.entity1.Users;
 
@@ -59,7 +61,7 @@ public class TestDB {
 		DeviceLocationInfo location = new DeviceLocationInfo();
 		location.setAcc(true);
 		location.setCellId(558);
-		location.setDate(new Timestamp(System.currentTimeMillis()));
+		location.setDate(new Timestamp(2017-1900,4,3,22,10,0,1));
 		location.setGpssstat("asdasd".getBytes());
 		location.setLac(418);
 		location.setLatitude(31.21645648);
@@ -71,6 +73,36 @@ public class TestDB {
 		location.setRealNot(false);
 		location.setSpeed(45141.5f);
 		locationdao.insertLocation(location);
-		
+	}
+	
+	@Test
+	public void TestDeviceLocationLBSInfo(){
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		context.start();
+		DeviceLocationLbsInfoDAO locationdao = (DeviceLocationLbsInfoDAO) context.getBean("DeviceLocationLbsInfoDAO");
+		DeviceLocationLbsInfo info = new DeviceLocationLbsInfo();
+		info.setCi1(1545);
+		info.setCi2(1546);
+		info.setCi3(1547);
+		info.setCi4(1548);
+		info.setCi5(1549);
+		info.setCi6(1550);
+		info.setCi7(1551);
+		info.setLac1(456);
+		info.setLac2(457);
+		info.setLac3(458);
+		info.setLac4(459);
+		info.setLac5(460);
+		info.setLac6(461);
+		info.setRssi(125);
+		info.setRssi2(126);
+		info.setRssi3(127);
+		info.setRssi4(128);
+		info.setRssi5(129);
+		info.setRssi6(130);
+		info.setRssi7(131);
+		info.setMcc(57);
+		info.setMnc(48);
+		locationdao.insertLbsInfo(info);
 	}
 }
